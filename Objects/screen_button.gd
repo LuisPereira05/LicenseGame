a26a4b1f53ddc458f77bafdae40390193078bef4
LuisPereira05 <luispereira.sl014@academico.ifsul.edu.br>
2024-09-10 21:58:58 -0300

    Click en los botones
    Por ahora solo cambia color.

--- Objects/screen_button.gd
+++ Objects/screen_button.gd
@@ -1,17 +1,29 @@
 extends AnimatedSprite2D
 
-
+var ontop : bool
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.frame = 0
 	pass # Replace with function body.
 
-func _on_area_2d_mouse() -> void: # Llamada cuando el mouse SALE/ENTRA al area del documento
-	if self.frame == 0:
+func _on_area_2d_mouse_entered() -> void:
+	self.frame = 1
+	ontop = true
+	pass # Replace with function body.
+
+
+func _on_area_2d_mouse_exited() -> void:
+	self.frame = 0
+	ontop = false
+	pass # Replace with function body.
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("Click") and ontop:
+		self.frame = 3
+	if Input.is_action_just_released("Click") and ontop:
 		self.frame = 1
-	elif self.frame == 1:
-		self.frame = 0
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func clicked(): #TODO Señal para pasar a la siguiente tab
 	pass
