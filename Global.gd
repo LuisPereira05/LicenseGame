0fb64a76af7cba9ae3da228ec79a36ad618336df
MaxGoxt <m.gomezcoitino@gmail.com>
2024-09-12 09:52:11 -0300

    Regreso al commit 13a824382

--- Global.gd
+++ Global.gd
@@ -2,4 +2,3 @@ extends Node
 
 var isdragging = false
 var sendInfo = {"license" : null}
-var current_tab: int
--- Scenes/ScreenManager.gd
+++ Scenes/ScreenManager.gd
@@ -3,36 +3,36 @@ extends Node2D
 @export var licTab : Node2D
 @export var infTab : Node2D
 
-var tabs : Array[Node]
+var tabs
+var curTab : int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	tabs = get_children(false)
-	global.current_tab = 0
-
+	tabs = Array(get_children(false))
+	print("size: ", tabs.size())
+	curTab = 0
+	to_tab(curTab)
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
 
 
-#func _on_button_was_clicked(action: String, buttonName : String) -> void:
-	#if action == "next":
-		#if buttonName != "next":
-			#global.sendInfo["license"] = buttonName
-		#curTab += 1
-		#to_tab(curTab)
-	#elif action == "back":
-		#curTab -= 1
-		#to_tab(curTab)
-	#print("license: ", global.sendInfo["license"], "\n")
+func _on_button_was_clicked(action: String, buttonName : String) -> void:
+	if action == "next":
+		if buttonName != "next":
+			global.sendInfo["license"] = buttonName
+		curTab += 1
+		to_tab(curTab)
+	elif action == "back":
+		curTab -= 1
+		to_tab(curTab)
+	print("license: ", global.sendInfo["license"], "\n")
 
-func _on_button_was_clicked(_action: String, buttonName : String, tab_index: int) -> void:
-	global.current_tab = tab_index
-	tabs.map(func(tab): 
-			if tab == tabs[tab_index] : 
-				tab.show() 
-			else: 
-				tab.hide())
-	if buttonName != "next":
-		global.sendInfo["license"] = buttonName
+func to_tab(index : int):
+	for i in range(0, tabs.size()):
+		if i != index:
+			tabs[i].hide()
+		else:
+			tabs[i].show()
