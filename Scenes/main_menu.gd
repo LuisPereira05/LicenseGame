b2ce9951578b7577aee4f7f725820ecd47c5038d
LuisPereira05 <luispereira.sl014@academico.ifsul.edu.br>
2024-09-26 15:29:11 -0300

    Main title update

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -2,10 +2,13 @@ extends Control
 
 @export var anim : AnimatedSprite2D
 @export var interface : MarginContainer
+@export var player : AnimationPlayer
 func _on_play_pressed():
 	anim.play()
 	interface.hide()
 
+func _ready() -> void:
+	player.play("Title")
 
 func _on_quit_pressed():
 	get_tree().quit()
